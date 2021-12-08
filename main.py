1035bc3ff1429bd25342b11e0da728d8992ca8a0
Sebastian Przesmycki <sprzesmycki@pgs-soft.com>
2021-12-08 18:48:42 +0100

    resolved some issues, added argparse, extracted methods

--- main.py
+++ main.py
@@ -5,20 +5,31 @@ import argparse
 
 
 def read_file(file_path, file_type):
-    rows = []
     with open(file_path) as data_file:
         if file_type == "csv":
-            reader = csv.reader(data_file)
-            next(reader)
-            for row in reader:
-                rows.append(row)
+            rows = read_csv_file(data_file)
         elif file_type == "json":
-            reader = json.load(data_file)
-            for row in reader:
-                rows.append([row.get('id'),
-                             row.get('first_name'),
-                             row.get('last_name'),
-                             row.get('weight') or "1"])
+            rows = read_json_file(data_file)
+    return rows
+
+
+def read_json_file(data_file):
+    rows = []  # nit: consider very big file with bilions of participants (for later)
+    reader = json.load(data_file)
+    for row in reader:
+        rows.append([row.get('id'),
+                     row.get('first_name'),
+                     row.get('last_name'),
+                     row.get('weight') or "1"])
+    return rows
+
+
+def read_csv_file(data_file):
+    rows = []  # nit: consider very big file with bilions of participants (for later)
+    reader = csv.reader(data_file)
+    next(reader)
+    for row in reader:
+        rows.append(row)  # how do we know it is the same order as in JSON?
     return rows
 
 
@@ -33,6 +44,13 @@ def get_winners(list_of_users, count):
     return random.choices(list_of_users, k=int(count))
 
 
+def show_winners(list_of_participants):
+    winners = get_winners_with_weight(list_of_participants, winners_count)
+    print("--- winners ---")
+    for winner in winners:
+        print(winner)
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -40,17 +58,11 @@ if __name__ == '__main__':
     parser.add_argument("--json", help="Provide json file path to process")
     args = parser.parse_args()
 
-    list_of_users_from_csv = read_file('data/participants2.csv',
-                                       'csv')  # use argparse or optparse modules to parametrize input
-    list_of_users_from_json = read_file('data/participants2.json', 'json')
-
     winners_count = input("How many winners?")
-    winners = get_winners_with_weight(list_of_users_from_csv, winners_count)
-    print("--- csv winners with weights ---")
-    for winner in winners:
-        print(winner)
 
-    winners = get_winners(list_of_users_from_json, winners_count)
-    print("--- json winners without weights ---")
-    for winner in winners:
-        print(winner)
+    if args.csv is not None:
+        list_of_users_from_csv = read_file(args.csv, 'csv')
+        show_winners(list_of_users_from_csv)
+    if args.json is not None:
+        list_of_users_from_json = read_file(args.json, 'json')
+        show_winners(list_of_users_from_json)
