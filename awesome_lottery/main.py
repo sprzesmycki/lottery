f4fecdd205cde6ce18d2c3566437b77501a235a2
Sebastian Przesmycki <sprzesmycki@pgs-soft.com>
2022-01-06 21:54:23 +0100

    first part of fixes, reading rewards is not working

--- awesome_lottery/main.py
+++ awesome_lottery/main.py
@@ -3,8 +3,9 @@ import json
 from pathlib import Path
 import random
 from dataclasses import dataclass
-
+from typing import List
 from dataclasses_json import dataclass_json
+from operator import attrgetter
 
 import click
 
@@ -15,6 +16,12 @@ class Participant:
     first_name: str
     last_name: str
     weight: int
+
+    def reduce_win_change(self):
+        self.weight = max(
+            self.weight - 1,
+            0
+        )
 
 
 @dataclass_json
@@ -29,117 +36,86 @@ class Prize:
 @dataclass
 class Prizes:
     name: str
-    prizes: list[Prize]
-    total_count: int = 0
+    prizes: List[Prize]
 
-    def get_prizes_count(self):
-        for i in self.prizes:
-            self.total_count += i.amount
-        return self.total_count
+    @property
+    def total_count(self) -> int:
+        return sum(
+            map(
+                attrgetter('amount'),
+                self.prizes
+            )
+        )
 
-    def get_next_prize(self):
-        for i in self.prizes:
-            if i.amount > 0:
-                i.amount -= 1
-                return i
+    def __iter__(self):
+        for prize in self.prizes:
+            for _ in range(prize.amount):
+                yield prize
 
 
 @dataclass_json
 @dataclass
 class Winner:
-    first_name: str
-    last_name: str
-    prize: str
+    participant: Participant
+    prize: Prize
 
 
 @dataclass_json
 @dataclass
 class Winners:
-    winners: list[Winner]
+    winners: List[Winner]
 
     def print_winners(self):
         print('Winners of lottery: ')
         for winner in self.winners:
-            print(f'-> {winner.first_name} {winner.last_name} won {winner.prize}')
+            print(f'-> {winner.participant.first_name} {winner.participant.last_name} won {winner.prize.name}')
         print('Congratulations for winners!')
 
 
 @dataclass_json
 @dataclass
 class Lottery:
-    participants: list[Participant]
-    prizes: list[Prizes]
-    winners: list[Winner]
+    participants: List[Participant]
+    prizes: List[Prizes]
+    winners: List[Winner]
+
+
+OPENERS = {
+    "csv": csv.DictReader,
+    "json": json.load,
+}
 
 
 def read_file(file_path, file_type):
+    opener = OPENERS[file_type]
     with open(file_path) as data_file:
-        if file_type == "csv":
-            return csv.DictReader(data_file)
-        elif file_type == "json":
-            return json.load(data_file)
-
-
-def read_participants_file(file_path, file_type):
-    with open(file_path) as data_file:
-        if file_type == "csv":
-            yield from read_participants_csv_file(data_file)
-        elif file_type == "json":
-            yield from read_participants_json_file(data_file)
-
-
-def read_participants_json_file(data_file):
-    reader = json.load(data_file)
-    yield from iterate_participants(reader)
-
-
-def read_participants_csv_file(data_file):
-    reader = csv.DictReader(data_file)
-    yield from iterate_participants(reader)
+        yield from opener(data_file)
 
 
 def iterate_participants(reader):
     for row in reader:
-        weight = row.get('weight')
-        if weight is None:
-            weight = 1
-        else:
-            weight = int(weight)
-        yield Participant(int(row.get('id')),
-                          row.get('first_name'),
-                          row.get('last_name'),
-                          weight)
+        idk = int(row.get('id'))
+        first_name = row.get('first_name')
+        last_name = row.get('last_name')
+        weight = int(row.get('weight') or 1)
+        yield Participant(idk, first_name, last_name, weight)
 
 
 def get_winner(list_of_users):
-    list_of_weights = list(map(lambda user: user.weight, list_of_users))
+    list_of_weights = list(map(attrgetter('weight'), list_of_users))
     return random.choices(list_of_users, weights=list_of_weights, k=1)[0]
-
-
-def reduce_winner_chances(winner):
-    winner.weight -= 1
 
 
 def get_extension(file):
     return Path(file).suffix[1:]
 
 
-def get_list_of_participants(participants):
-    list_of_participants = []
-    it = iter(participants)
-    while True:
-        try:
-            list_of_participants.append(next(it))
-        except StopIteration:
-            break
-    return list_of_participants
-
-
 def get_winners_with_prizes(winners, prizes: Prizes):
     list_of_winners_with_prizes = []
+    iter__ = prizes.__iter__()
     for winner in winners:
         list_of_winners_with_prizes.append(
-            Winner(first_name=winner.first_name, last_name=winner.last_name, prize=prizes.get_next_prize().name))
+            Winner(participant=winner, prize=next(iter__)))
     return Winners(winners=list_of_winners_with_prizes)
 
 
@@ -147,31 +123,48 @@ def get_first_file_in_path(path):
     return next(Path(f"{path}/").iterdir())
 
 
+def validate_file(ctx, param, value):
+    if isinstance(value, tuple):
+        return value
+
+    try:
+        return value
+    except ValueError:
+        raise click.BadParameter('-- File not specified --')
+
+
+def validate_rewards(ctx, param, value):
+    if isinstance(value, tuple):
+        return value
+
+    try:
+        return get_first_file_in_path('data/lottery_templates')
+    except ValueError:
+        raise click.BadParameter("-- File doesn't exist --")
+
+
 @click.command()
-@click.option('--file', help='Provide file path to process')
-@click.option('--filetype', help='Provide file type csv/json')
-@click.option('--rewards', help='Provide path for file with rewards')
+@click.option('--file', help='Provide file path to process',
+              type=click.UNPROCESSED, callback=validate_file, prompt=True)
+@click.option('--filetype', help='Provide file type csv/json', type=click.Choice(['csv', 'json'], case_sensitive=False))
+@click.option('--rewards', help='Provide path for file with rewards',
+              type=click.UNPROCESSED, callback=validate_rewards)
 @click.option('--results', help='Provide filename to save results')
 def lottery(file, filetype, rewards, results):
-    participants = []
-    if file is not None:
-        filetype = filetype or get_extension(file)
-        participants = read_participants_file(file, filetype)
-    else:
-        print('-- File not specified --')
-        exit()
-    participants = get_list_of_participants(participants)
+    filetype = filetype or get_extension(file)
+    rows = read_file(file, filetype)
+    participants = list(iterate_participants(rows))
 
-    if rewards is None:
-        rewards = get_first_file_in_path('data/lottery_templates')
     prizes = read_file(rewards, 'json')
+    for prize in prizes:
+        print("")
     rewards = Prizes.from_json(json.dumps(prizes))
 
     winners = []
-    for i in range(rewards.get_prizes_count()):
+    for i in range(rewards.total_count):
         winner = get_winner(participants)
         winners.append(winner)
-        reduce_winner_chances(winner)
+        winner.reduce_win_change()
 
     lottery_data = Lottery(participants=participants, prizes=prizes, winners=winners)
     lottery_json = Lottery.to_json(lottery_data)
